f7cb09af81e4170eb7811c0d02e2d03ddd91aee5
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2022-12-25 17:50:20 -0800

    Finished day23

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashSet, VecDeque};
+use std::time::Instant;
 use std::{fs::File, io::Read};
 
 #[derive(Debug, Hash, Eq, PartialEq, Clone, Copy)]
@@ -112,6 +113,25 @@ impl Ground {
             println!();
         }
     }
+
+    fn empty_ground_tiles(&self) -> usize {
+        let max_x = self.elves.iter().map(|elf| elf.x).max().unwrap();
+        let min_x = self.elves.iter().map(|elf| elf.x).min().unwrap();
+        let max_y = self.elves.iter().map(|elf| elf.y).max().unwrap();
+        let min_y = self.elves.iter().map(|elf| elf.y).min().unwrap();
+
+        let mut ground_tiles = 0;
+
+        for row in min_y..=max_y {
+            for col in min_x..=max_x {
+                if !self.elves.contains(&Point { x: col, y: row }) {
+                    ground_tiles += 1;
+                }
+            }
+        }
+
+        ground_tiles
+    }
 }
 
 #[derive(Debug)]
@@ -143,7 +163,7 @@ fn part1(contents: &mut String) {
         Direction::East,
     ]);
 
-    static NUM_ROUNDS: usize = 2;
+    static NUM_ROUNDS: usize = 10;
     for round in 0..NUM_ROUNDS {
         println!("Start of proposals");
         println!("Ground: {:?}", ground);
@@ -152,14 +172,19 @@ fn part1(contents: &mut String) {
 
         let mut proposals = Vec::<Option<Point>>::new();
 
+        let mut num_cant_move = 0;
+
         for elf in ground.elves.iter() {
             let adjacent = ground.adjacents(&elf);
             if adjacent.blank() {
                 println!("No neighbors");
+                num_cant_move += 1;
                 proposals.push(None);
                 continue;
             }
             println!("Adjacent to point: {:?}: {:?}", elf, adjacent);
+
+            let mut had_match = false;
 
             for (ind, direction) in directions.iter().enumerate() {
                 match direction {
@@ -169,6 +194,7 @@ fn part1(contents: &mut String) {
                                 x: elf.x,
                                 y: elf.y - 1,
                             }));
+                            had_match = true;
                             break;
                         }
                     }
@@ -178,6 +204,7 @@ fn part1(contents: &mut String) {
                                 x: elf.x,
                                 y: elf.y + 1,
                             }));
+                            had_match = true;
                             break;
                         }
                     }
@@ -187,6 +214,7 @@ fn part1(contents: &mut String) {
                                 x: elf.x - 1,
                                 y: elf.y,
                             }));
+                            had_match = true;
                             break;
                         }
                     }
@@ -196,14 +224,23 @@ fn part1(contents: &mut String) {
                                 x: elf.x + 1,
                                 y: elf.y,
                             }));
+                            had_match = true;
                             break;
                         }
                     }
                 };
             }
 
-            directions.rotate_left(1);
+            if !had_match {
+                proposals.push(None);
+            }
         }
+
+        if num_cant_move == ground.elves.len() {
+            panic!("No elf can move");
+        }
+
+        directions.rotate_left(1);
 
         assert!(proposals.len() == ground.elves.len());
 
@@ -223,17 +260,149 @@ fn part1(contents: &mut String) {
         ground.display();
     }
 
-    println!("Ground: {:?}", ground);
+    //println!("Ground: {:?}", ground);
+    println!("Empty ground tiles: {}", ground.empty_ground_tiles());
 }
 
-fn part2(contents: &mut String) {}
+fn part2(contents: &mut String) {
+    let mut ground = Ground::new();
+
+    for (ri, row) in contents.lines().enumerate() {
+        for (ci, col) in row.chars().enumerate() {
+            if col == '#' {
+                ground.elves.push(Point {
+                    x: ci as isize,
+                    y: ri as isize,
+                });
+            }
+        }
+    }
+
+    let mut directions = VecDeque::from([
+        Direction::North,
+        Direction::South,
+        Direction::West,
+        Direction::East,
+    ]);
+
+    let mut round = 0;
+    loop {
+        //println!("Started round {}", round);
+
+        //println!("Start of proposals");
+        //let start = Instant::now();
+        //println!("Ground: {:?}", ground);
+
+        //ground.display();
+
+        let mut proposals = Vec::<Option<Point>>::with_capacity(ground.elves.len());
+
+        for elf in ground.elves.iter() {
+            let adjacent = ground.adjacents(&elf);
+            if adjacent.blank() {
+                //println!("No neighbors");
+                proposals.push(None);
+                continue;
+            }
+            //println!("Adjacent to point: {:?}: {:?}", elf, adjacent);
+
+            let mut had_match = false;
+
+            for direction in directions.iter() {
+                match direction {
+                    Direction::North => {
+                        if !adjacent.n && !adjacent.ne && !adjacent.nw {
+                            proposals.push(Some(Point {
+                                x: elf.x,
+                                y: elf.y - 1,
+                            }));
+                            had_match = true;
+                            break;
+                        }
+                    }
+                    Direction::South => {
+                        if !adjacent.s && !adjacent.se && !adjacent.sw {
+                            proposals.push(Some(Point {
+                                x: elf.x,
+                                y: elf.y + 1,
+                            }));
+                            had_match = true;
+                            break;
+                        }
+                    }
+                    Direction::West => {
+                        if !adjacent.w && !adjacent.nw && !adjacent.sw {
+                            proposals.push(Some(Point {
+                                x: elf.x - 1,
+                                y: elf.y,
+                            }));
+                            had_match = true;
+                            break;
+                        }
+                    }
+                    Direction::East => {
+                        if !adjacent.e && !adjacent.ne && !adjacent.se {
+                            proposals.push(Some(Point {
+                                x: elf.x + 1,
+                                y: elf.y,
+                            }));
+                            had_match = true;
+                            break;
+                        }
+                    }
+                };
+            }
+
+            if !had_match {
+                proposals.push(None);
+            }
+        }
+
+        directions.rotate_left(1);
+
+        //println!("End of proposals, took: {:?}", start.elapsed());
+        //println!("Len of proposals is {}", proposals.len());
+
+        assert!(proposals.len() == ground.elves.len());
+
+        //println!("Starting to move elves");
+        //let start = Instant::now();
+
+        let mut cant_move = 0;
+
+        for (elf_ind, proposal) in proposals.iter().enumerate() {
+            if proposals.iter().filter(|prop| *prop == proposal).count() > 1 {
+                cant_move += 1;
+                continue;
+            }
+
+            if let Some(new_point) = proposal {
+                ground.elves[elf_ind] = *new_point;
+            } else {
+                cant_move += 1;
+            }
+        }
+
+        if cant_move == proposals.len() {
+            break;
+        }
+
+        //println!("Moving elves took {:?}", start.elapsed());
+
+        //println!("Points are now: {:?}", ground.elves);
+        //ground.display();
+        round += 1;
+    }
+
+    println!("First round where no elf can move: {}", round + 1);
+}
 
 fn main() -> std::io::Result<()> {
     let mut f = File::open("input")?;
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    part1(&mut contents);
+    //part1(&mut contents);
     part2(&mut contents);
 
     Ok(())

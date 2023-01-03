b699c10ad12478d738c20b0e1a5c6140e69f212c
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2023-01-03 00:53:50 -0800

    Day16 part1

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,3 +1,5 @@
+#![feature(binary_heap_retain)]
+
 use std::collections::{BinaryHeap, HashMap, HashSet};
 use std::str::FromStr;
 use std::{fs::File, io::Read};
@@ -18,12 +20,13 @@ struct Cost {
 }
 
 impl Cost {
-    fn update_pressure(&mut self, valves: &HashMap<String, Valve>) {
-        for (valve, open) in self.valve_statuses.iter() {
-            if *open {
-                self.pressure_dispersed += valves[valve].flow_rate;
+    fn is_open(&self, name: &String) -> bool {
+        for (valve_name, open) in self.valve_statuses.iter() {
+            if *valve_name == *name {
+                return *open;
             }
         }
+        panic!("Did not find valve {}", name);
     }
 
     /// open_valve returns a bool that says if the value in the cost was modified
@@ -36,6 +39,34 @@ impl Cost {
             }
         }
         panic!("Could not find target valve: {}", name);
+    }
+
+    fn has_open_zero(&self, costs: &Vec<(usize, String)>) -> bool {
+        for (cost, valve_name) in costs {
+            if *cost == 0 && self.is_open(valve_name) {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn maximum_potential(&self, costs: &Vec<(usize, String)>) -> usize {
+        let mut potential = 0;
+        let mut current_time = self.time_spent;
+        let mut costs = costs.clone();
+        costs.reverse();
+        for (flow_rate, name) in costs {
+            if !self.is_open(&name) {
+                if current_time < MAX_TIME {
+                    potential += (MAX_TIME - current_time) * flow_rate;
+                    current_time += 1;
+                }
+            }
+        }
+
+        //println!("Potential for {:?} is {}", self, potential);
+
+        potential
     }
 }
 
@@ -51,9 +82,15 @@ impl Ord for Cost {
     }
 }
 
-pub static TIME_LEFT: usize = 30;
+static MAX_TIME: usize = 30;
 
 fn part1(valves: &HashMap<String, Valve>) {
+    let mut costs = valves
+        .iter()
+        .map(|(name, data)| (data.flow_rate, name.clone()))
+        .collect::<Vec<_>>();
+    costs.sort();
+
     let mut states = BinaryHeap::from([Cost {
         current_valve: "AA".to_string(),
         pressure_dispersed: 0,
@@ -64,22 +101,45 @@ fn part1(valves: &HashMap<String, Valve>) {
             .collect::<Vec<_>>(),
     }]);
 
-    let mut history = HashSet::new();
+    let mut history = HashSet::<Cost>::new();
 
-    let mut found_path = false;
+    let mut found_max = false;
     let mut max_dispersed = 0;
+
+    let mut dispersed = usize::MAX;
 
     while !states.is_empty() {
         let current = states.pop().unwrap();
 
         //println!("Current state: {:?}", current);
+        dispersed -= 1;
+        if dispersed == 0 {
+            panic!("Max iter");
+        }
 
-        if current.time_spent == 30 {
+        if found_max
+            && current.maximum_potential(&costs) + current.pressure_dispersed < max_dispersed
+        {
+            continue;
+        }
+
+        if current.has_open_zero(&costs) {
+            continue;
+        }
+
+        if current.time_spent == MAX_TIME {
             if current.pressure_dispersed > max_dispersed {
-                if !found_path {
-                    found_path = true;
+                if !found_max {
+                    found_max = true;
                 }
                 max_dispersed = current.pressure_dispersed;
+                history.retain(|state| {
+                    state.maximum_potential(&costs) + state.pressure_dispersed >= max_dispersed
+                });
+                states.retain(|state| {
+                    state.maximum_potential(&costs) + state.pressure_dispersed >= max_dispersed
+                });
+                println!("{:?}", current);
                 println!("Max is now {}", max_dispersed);
             }
             continue;
@@ -91,15 +151,16 @@ fn part1(valves: &HashMap<String, Valve>) {
 
         // Add one state for opening the current valve
         let mut newly_opened = current.clone();
-        newly_opened.open_valve(&current.current_valve);
-        newly_opened.update_pressure(valves);
         newly_opened.time_spent += 1;
+        if newly_opened.open_valve(&current.current_valve) {
+            newly_opened.pressure_dispersed +=
+                valves[&current.current_valve].flow_rate * (MAX_TIME - newly_opened.time_spent);
+        }
         states.push(newly_opened);
 
-        for (valve, _) in current.valve_statuses.iter() {
+        for valve in valves[&current.current_valve].leads_to.iter() {
             let mut other_valve = current.clone();
             other_valve.current_valve = valve.clone();
-            other_valve.update_pressure(valves);
             other_valve.time_spent += 1;
             states.push(other_valve);
         }
